d01e328210c0df57d14c67de15302f04a67050c8
zascho-bbc <oliver.aschwanden@bbcag.ch>
2016-09-07 11:02:59 +0200

    Hi

--- app/src/main/java/com/example/zorien/grademanager/MainActivity.java
+++ app/src/main/java/com/example/zorien/grademanager/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
+    //Hello it's me
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
