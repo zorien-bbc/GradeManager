b91f70fade78a7c14f37fc5fc23e32c30b32b429
oliii <oliver.Aschwanden@bbcag.ch>
2016-09-08 10:24:53 +0200

    Fach

---
+++ app/src/main/java/com/example/zorien/grademanager/Model/Fach.java
@@ -0,0 +1,53 @@
+package com.example.zorien.grademanager.model;
+
+import java.util.ArrayList;
+
+/**
+ * Created by zascho on 08.09.2016.
+ */
+public class Fach {
+    private int fachid;
+    private String fachkurz;
+    private String bezeichnung;
+    private ArrayList<Kurs> kurse;
+    //caner
+    public Fach() {
+    }
+
+    private Boolean selectFachByID(int currentID){return false;}
+    public Boolean updateFach(int currentID){return false;}
+    public int insertFach(){return 0;}
+    public Boolean deleteFach(){return false;}
+
+    public String getFachkurz() {
+        return fachkurz;
+    }
+
+    public void setFachkurz(String fachkurz) {
+        this.fachkurz = fachkurz;
+    }
+
+    public String getBezeichnung() {
+        return bezeichnung;
+    }
+
+    public void setBezeichnung(String bezeichnung) {
+        this.bezeichnung = bezeichnung;
+    }
+
+    public ArrayList<Kurs> getKurse() {
+        return kurse;
+    }
+
+    public void setKurse(ArrayList<Kurs> kurse) {
+        this.kurse = kurse;
+    }
+
+    public int getFachid() {
+        return fachid;
+    }
+
+    public void setFachid(int fachid) {
+        this.fachid = fachid;
+    }
+}
---
+++ app/src/main/java/com/example/zorien/grademanager/Model/Kurs.java
@@ -0,0 +1,8 @@
+package com.example.zorien.grademanager.model;
+
+/**
+ * Created by zascho on 08.09.2016.
+ */
+public class Kurs {
+
+}
